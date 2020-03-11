10285911dccb217da5305ba871f0e8e41ecf498c
Tetiana Ovchar <tetianaovchar@gmail.com>
2020-03-11 11:08:12 +0200

    feat: solve task what season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,15 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date === undefined) return 'Unable to determine the time of year!';
+  if (!date instanceof Date ||date.getTime() === undefined )  throw Error; // typeof   -> object; i.constructor === Date -> true
+ // one of functions that was not  in pseudo time object  for very tricky case check
+  const month = date.getMonth(); // 0-11 months
+  if (month > 10) return 'winter';
+  if (month > 7) return 'autumn';
+  if (month > 4) return 'summer';
+  if (month > 1) return 'spring';
+  if (month > -1) return 'winter';
 };
+
+// OR create array w,w,s etc 0-11 and return form there
+
+
