fd2024509fc95b04529e5f40ecbbe603d7d4ea17
Tetiana Ovchar <tetianaovchar@gmail.com>
2020-03-11 11:04:18 +0200

    feat: solve hanoi tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,13 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed ) {
+    const turns = Math.pow(2,disksNumber) - 1;
+    const time = turns/(turnsSpeed/3600);
+    const answer =  {
+      turns: turns,
+      seconds: time
+    };
+    return answer;
 }
+
+// const time = turns/turnsSpeed*3600;  gave error for one test case
+   //-  "seconds": 1.6502813140731935e+217
+  //+  "seconds": 1.6502813140731933e+217
