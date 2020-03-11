dec1e564550f2679ef8b881ef4552a8a0ce64c48
Tetiana Ovchar <tetianaovchar@gmail.com>
2020-03-11 11:00:27 +0200

    feat: solve task carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,22 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+const radioactiveDecayConst = 0.693 / HALF_LIFE_PERIOD;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') return false;
+  sampleActivity = parseFloat(sampleActivity);
+  if (sampleActivity <= 0 ||
+    sampleActivity > MODERN_ACTIVITY ||
+    sampleActivity === undefined ||
+    isNaN(sampleActivity)) return false //  type of NaN is number,so unsuitable; NaN === NaN false, so isNaN to be used
+  const ratioActivities = MODERN_ACTIVITY / sampleActivity;
+  let result = Math.ceil(Math.log(ratioActivities) / radioactiveDecayConst);
+  return result;
+  //  alternative way, inverted conditons, to return false
+  //return (
+  //  sampleActivity > 0 &&
+  //  sampleActivity < MODERN_ACTIVITY &&
+  //  sampleActivity !== undefined &&
+  //  !isNaN(sampleActivity) &&
+  //  result);
 };
