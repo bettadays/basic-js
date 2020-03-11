46dbcfb30ada5c18132989019ca21d3dea8639b5
Tetiana Ovchar <tetianaovchar@gmail.com>
2020-03-11 11:06:35 +0200

    feat: solve task transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,37 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+  if (!Array.isArray(arr)) throw new Error();
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--double-prev':
+        if (arr[i - 1] !== undefined) {
+          result.push(arr[i - 1]);
+        }
+        break;
+
+      case '--double-next':
+        if (arr[i + 1] !== undefined) {
+          result.push(arr[i + 1]);
+        }
+        break;
+
+      case '--discard-next':
+        if (arr[i + 1] !== undefined) {
+          i++;
+        }
+        break;
+
+      case '--discard-prev':
+        if (arr[i - 1] !== undefined) {
+          result.pop();
+        }
+        break;
+
+      default:
+        result.push(arr[i]);
+    }
+  }
+  return result;
 };
