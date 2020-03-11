0ae0b7d6e22b732a59edee4d27c4c2b3691f4abb
Tetiana Ovchar <tetianaovchar@gmail.com>
2020-03-11 11:05:09 +0200

    feat: solve task recursive depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,11 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+  calculateDepth(arr) {
+    let depth = 0;
+    arr.forEach(item => {
+      if (Array.isArray(item)) {
+        depth = Math.max(this.calculateDepth(item), depth); // this!!!
+      }
+    })
+    return depth + 1;
+  }
 };
