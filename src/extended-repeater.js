02e7405163ea180149f8447e8709d571b67eff20
Tetiana Ovchar <tetianaovchar@gmail.com>
2020-03-11 11:01:52 +0200

    feat: solve task hanoi tower

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,20 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports =   function repeater(str,  {   //to use defaults used destructuring
+    repeatTimes = 1,
+    separator = '+',
+    addition = '',
+    additionRepeatTimes = 1, // to have base
+    additionSeparator = '|'
+  } = {}) { // default params
+
+  if (typeof str !== 'string') str = str + '';
+  if (typeof addition !== 'string') addition = addition + ''; //[object Object],// if toString [object Null] [object Undefined]
+  let result = '';
+  for( let i = repeatTimes; i > 0; i--) {
+    result += str;  // !+
+    for( let i = additionRepeatTimes; i > 0; i--) {
+       result += addition + ( i > 1 ? additionSeparator : '');
+    }
+    result += ( i > 1 ? separator : '');
+  }
+  return result;
 };
-  
