3425de23cdf699fee3a29b176fb61d7e48c70b01
Tetiana Ovchar <tetianaovchar@gmail.com>
2020-03-11 11:01:11 +0200

    feat: solve task dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,14 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  const result = [];
+  if(!Array.isArray(members))  return false;
+  // also !members instanceof Array | members.constructor !== Array
+  members.forEach(item => {
+    if(typeof item === 'string') {  //map with filter could be also used
+    item = item.trim().toUpperCase(); //  first to upper case, after sort, but before result arr creation, since its a string method
+    result.push(item[0]);
+   }
+  })
+   return result.sort().join('')
 };
+
+
